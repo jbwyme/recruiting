2b982febf373752cdb970de47cfd893f65cb340d
jbwyme <josh@mixpanel.com>
2014-11-13 14:47:52 -0800

    refactorings

--- lnkd/crawl.py
+++ lnkd/crawl.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import config
 import cookielib
+import fcntl
 import os
 import random
 import re
@@ -14,44 +15,77 @@ import urllib2
 class LinkedInCrawler(object):
 
     def __init__(self, login, password):
-        self.MAX_CRAWL_DEPTH = 5
+        self.crawls_per_run = 100
         self.login = login
         self.password = password
-        self.profile_ids = []
-        self.profile_creds = []
-        if os.path.isfile('profiles'):
-            with open('profiles', 'r') as f:
-                print "loading existing profiles..."
-                for line in f.readlines():
-                    profile_id = re.search('id=([0-9]+)', line).group(1)
-                    cred = line.strip()
-                    self.profile_ids.append(profile_id)
-                    self.profile_creds.append(cred)
+        self.all_profile_ids = []
+        self.cred_queue = []
+        # lock and grab top n profiles to crawl
+        if not os.path.isfile('.crawl.lck'):
+            with open('.crawl.lck', 'a'):
+                os.utime('.crawl.lck', None)
+        lock_file = open(".crawl.lck","r")
+        try:
+            fcntl.flock(lock_file.fileno(),fcntl.LOCK_EX)
+            if os.path.isfile('queue'):
+                os.rename('queue', 'queue.old')
+                with open('queue.old', 'r') as f:
+                    print "loading existing profiles..."
+                    line_num = 0
+                    with open('queue', 'w+') as f2:
+                        for line in f.readlines():
+                            if line_num < self.crawls_per_run: # grab first n rows to crawl during this run
+                                self.cred_queue.append(line.strip())
+                            else: # write the rest back to the queue file
+                                f2.write(line)
+                            self.all_profile_ids.append(int(re.search('id=([0-9]+)', line).group(1)))
+                            line_num += 1
+                os.remove('queue.old')
+        except:
+            if os.path.isfile('queue'):
+                os.remove('queue')
+            if os.path.isfile('queue.old'):
+                os.rename('queue.old', 'queue')
+            raise
+        finally:
+            lock_file.close()
 
-        # SQLite
-        self.conn = sqlite3.connect('lnkd.db')
-        self.conn.row_factory = sqlite3.Row
-        self.createDb()
+        try:
+            # SQLite
+            self.conn = sqlite3.connect('lnkd.db')
+            self.conn.row_factory = sqlite3.Row
+            self.createDb()
 
-        # Simulate browser with cookies enabled
-        self.cj = cookielib.MozillaCookieJar('parser.cookie.txt')
-        self.opener = urllib2.build_opener(
-            urllib2.HTTPRedirectHandler(),
-            urllib2.HTTPHandler(debuglevel=0),
-            urllib2.HTTPSHandler(debuglevel=0),
-            urllib2.HTTPCookieProcessor(self.cj)
-        )
+            # Simulate browser with cookies enabled
+            self.cj = cookielib.MozillaCookieJar('parser.cookie.txt')
+            self.opener = urllib2.build_opener(
+                urllib2.HTTPRedirectHandler(),
+                urllib2.HTTPHandler(debuglevel=0),
+                urllib2.HTTPSHandler(debuglevel=0),
+                urllib2.HTTPCookieProcessor(self.cj)
+            )
 
-        self.opener.addheaders = [
-            ('User-agent', ('Mozilla/5.0 (Macintosh; '
-                           'Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36)'))
-        ]
+            self.opener.addheaders = [
+                ('User-agent', ('Mozilla/5.0 (Macintosh; '
+                               'Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36)'))
+            ]
 
-        self.loginPage()
-        self.crawlProfile()
-        #self.crawlProfiles()
-        self.conn.close()
+            self.loginPage()
+            print self.cred_queue
+            for cred in self.cred_queue[:]:
+                with open('queue', 'a+') as f:
+                    f.write(cred + '\n')
+                self.cred_queue.remove(cred)
+                self.crawlProfile(cred)
 
+            #self.crawlProfiles()
+            self.conn.close()
+        except:
+            print 'writing pending buffer (%d profiles) to persistent queue' % len(self.cred_queue)
+            for cred in self.cred_queue:
+                with open('queue', 'a+') as f:
+                    f.write(cred + '\n')
+            raise
 
     def loadPage(self, url, data=None, retry_num=0):
         try:
@@ -85,67 +119,48 @@ class LinkedInCrawler(object):
             form = soup.find('form')
             for hi in form.select('input[type=hidden]'):
                 post_data[hi['name']] = hi['value']
+            self._sendMail("LinkedIn is asking for verification code", "you should probably stop your crons")
             code = raw_input("Please enter your verification code: ")
             post_data['PinVerificationForm_pinParam'] = code
             verification_data = urllib.urlencode(post_data)
             html = self.loadPage("https://www.linkedin.com/uas/ato-pin-challenge-submit", verification_data)
 
+
     def createDb(self):
         c = self.conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS profile
-                     (profile_id real PRIMARY KEY, name text, location text, title text, company text, description text, url text, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME)''')
+                     (profile_id real PRIMARY KEY, name text, location text, title text, company text, description text, url text, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, last_crawled_at DATETIME, updated_at DATETIME)''')
         self.conn.commit()
         c.close()
 
-    def crawlProfile(self, url=None, depth=0):
-        url = url or 'https://www.linkedin.com/profile/view?id=48265010&authType=name&authToken=P42Z&offset=23&trk=prof-sb-pdm-similar-photo'
-        print 'crawling "%s" at depth %d' % (url, depth)
+
+    def crawlProfile(self, cred=None):
+        url = 'https://www.linkedin.com/profile/view?%s' % cred if cred else 'id=48265010&authType=name&authToken=P42Z'
+        print 'crawling "%s"' % url
         profile_id = int(re.search('id=([0-9]+)', url).group(1))
         html = self.loadPage(url)
         self._delay()
         self._saveProfile(profile_id, url, html)
         soup = BeautifulSoup(html)
         discovery_results = soup.find(class_='discovery-results')
-        if depth < self.MAX_CRAWL_DEPTH and discovery_results is not None:
-            similar_urls = [a['href'] for a in discovery_results.findAll('a') if '/profile/view' in a['href']]
-            for surl in similar_urls:
-                profile_id = re.search('id=([0-9]+)', surl).group(1)
+        if discovery_results is not None:
+            similar_profiles = [a['href'] for a in discovery_results.findAll('a') if '/profile/view' in a['href']]
+            for profile_url in similar_profiles:
+                profile_id = int(re.search('id=([0-9]+)', profile_url).group(1))
                 authType = ''
                 authToken = ''
                 try:
-                    authType = re.search('authType=([a-zA-Z_0-9]+)', surl).group(1)
-                    authToken = re.search('authToken=([a-zA-Z_0-9]+)', surl).group(1)
+                    authType = re.search('authType=([a-zA-Z_0-9]+)', profile_url).group(1)
+                    authToken = re.search('authToken=([a-zA-Z_0-9]+)', profile_url).group(1)
                 except:
                     pass
-                if profile_id not in self.profile_ids:
+                if profile_id not in self.all_profile_ids:
                     cred = 'id=%s&authType=%s&authToken=%s' % (profile_id, authType, authToken)
-                    self.profile_ids.append(profile_id)
-                    self.profile_creds.append(cred)
+                    self.all_profile_ids.append(profile_id)
                     with open('profiles', 'a+') as f:
                         f.write(cred + '\n')
-                    print 'found url: %s' % cred
-            for surl in similar_urls:
-                self.crawlProfile(surl, depth + 1)
+                    print 'adding profile to crawl: %s' % cred
 
-    def crawlProfiles(self):
-        successes = 0
-        failures = 0
-        for cred in self.profile_creds:
-            try:
-                profile_id = int(re.search('id=([0-9]+)', cred).group(1))
-                url = 'https://www.linkedin.com/profile/view?%s' % cred
-                html = self.loadPage(url)
-                self._saveProfile(profile_id, url, html)
-                successes += 1
-            except Exception as e:
-                import traceback
-                failures += 1
-                print 'profile %d - %s: failed' % (profile_id, profile['name'] if 'name' in profile else 'unknown')
-                print "Exception: %s, Trace: %s" % (e, traceback.format_exc())
-
-            self._delay()
-
-        print 'successes: %d, changes: %d, failures %d' % (successes, changes, failures)
 
     def _saveProfile(self, profile_id, url, html):
         soup = BeautifulSoup(html)
@@ -169,38 +184,43 @@ class LinkedInCrawler(object):
             else:
                 profile['description'] = current_position.select('.description')[0].get_text()
 
-        # compare to last run
-        c.execute('SELECT * FROM profile WHERE profile_id = ?', (profile_id,))
-        saved_profile = c.fetchone()
-        description_changed = saved_profile is not None and saved_profile['description'] != profile['description']
+                # compare to last run
+                c.execute('SELECT * FROM profile WHERE profile_id = ?', (profile_id,))
+                saved_profile = c.fetchone()
+                description_changed = saved_profile is not None and saved_profile['description'] != profile['description']
 
-        # e-mail if the description changed
-        if description_changed:
-            FROM = "jbwyme@gmail.com"
-            TO = ["josh@mixpanel.com"]
-            SUBJECT = "Job description changed for %s" % profile['name'].encode('utf-8')
-            TEXT = 'url: %s\n\nold:\n---------\n%s\n\nnew:\n---------\n%s' % (url, saved_profile['description'].encode('utf-8'), profile['description'].encode('utf-8'))
-            message = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (FROM, ",".join(TO), SUBJECT, TEXT)
-            server = smtplib.SMTP('smtp.gmail.com',587) # port 465 or 587
-            server.ehlo()
-            server.starttls()
-            server.ehlo()
-            server.login(config.GMAIL_EMAIL, config.GMAIL_PASSWORD)
-            server.sendmail(FROM, TO, message)
-            server.close()
+                # e-mail if the description changed
+                if description_changed:
+                    self._sendMail("Job description changed for %s" % profile['name'].encode('utf-8'), 
+                        'url: %s\n\nold:\n---------\n%s\n\nnew:\n---------\n%s' % (url, saved_profile['description'].encode('utf-8'), profile['description'].encode('utf-8')))
 
-        # save profile to db
-        if saved_profile is None or description_changed:
-            c.execute("INSERT OR IGNORE INTO profile (profile_id, name, url) VALUES (?, ?, ?)", (profile_id, profile['name'], url))
-            c.execute("UPDATE profile SET name = ?, url = ?, location = ?, title = ?, company = ?, description = ?, updated_at = CURRENT_TIMESTAMP WHERE profile_id = ?",
-                (profile['name'], url, profile['location'], profile['title'], profile['company'], profile['description'], profile_id,))
-            self.conn.commit()
-            print 'profile %d - %s: profile %s' % (profile_id, profile['name'], 'updated' if description_changed else 'added')
+                # save profile to db
+                if saved_profile is None or description_changed:
+                    c.execute("INSERT OR IGNORE INTO profile (profile_id, name, url) VALUES (?, ?, ?)", (profile_id, profile['name'], url))
+                    c.execute("UPDATE profile SET name = ?, url = ?, location = ?, title = ?, company = ?, description = ?, updated_at = CURRENT_TIMESTAMP WHERE profile_id = ?",
+                        (profile['name'], url, profile['location'], profile['title'], profile['company'], profile['description'], profile_id,))
+                    print 'profile %d - %s: profile %s' % (profile_id, profile['name'], 'updated' if description_changed else 'added')
+
+                c.execute("UPDATE profile SET last_crawled_at = CURRENT_TIMESTAMP WHERE profile_id = ?", (profile_id,))
+                self.conn.commit()
         c.close()
 
 
+    def _sendMail(self, subject, body):
+        FROM = "jbwyme@gmail.com"
+        TO = ["josh@mixpanel.com"]
+        message = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (FROM, ",".join(TO), subject, body)
+        server = smtplib.SMTP('smtp.gmail.com',587) # port 465 or 587
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(config.GMAIL_EMAIL, config.GMAIL_PASSWORD)
+        server.sendmail(FROM, TO, message)
+        server.close()
+
+
     def _delay(self):
-        delay = random.randrange(1,15,1)
+        delay = random.randrange(2,10,1)
         print 'waiting %d seconds...' % delay
         time.sleep(delay) # random delay
 
